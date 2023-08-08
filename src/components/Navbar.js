de62512dd40b337d803a424f720ea51cced07780
meryemsanem <meryemsanemkucukbayram@gmail.com>
2023-08-08 14:00:19 +0300

    Style navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { AiFillSetting } from 'react-icons/ai';
 import { BiMicrophone } from 'react-icons/bi';
 import { IoEarth } from 'react-icons/io5';
+import './styles/Navbar.css';
 
 function Navbar() {
   return (
     <nav>
       <IoEarth />
       <p>Let&apos;s Explore Countries!</p>
-      <div>
+      <div className="buttons">
         <BiMicrophone />
         <AiFillSetting />
       </div>
